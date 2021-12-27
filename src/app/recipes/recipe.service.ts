a647cd27c41f957534ce3ad2016201ba42a49792
zinaelnahel <zina.el.nahel@accenture.com>
2021-12-27 19:19:34 +0100

    add recipe details

---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,26 @@
+
+import { EventEmitter, Injectable } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
+import { ShoppingListService } from "../shopping-list/shopping-list.service";
+import { Recipe } from "./recipe.model";
+@Injectable()
+export class RecipeService {
+  recipeSelected= new EventEmitter<Recipe>();
+
+  private recipes: Recipe[]=[
+        new Recipe('Schnitzel', 
+        'This is simply a test',
+         'https://images.unsplash.com/photo-1560611588-163f295eb145?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80', 
+         [new Ingredient('Meat', 1), new Ingredient ( ' French fries ', 20) ]),
+        new Recipe('Burger', 'This is simply a test', 
+        'https://images.unsplash.com/photo-1568901346375-23c9450c58cd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1299&q=80', 
+        [ new Ingredient ( 'Buns',2 ), new Ingredient ( 'Meat',2 )])
+      ];
+  constructor(private slService : ShoppingListService ){}
+   getRecipes(){
+     return this.recipes.slice();
+   }   
+   addIngredientsToShoppingList(ingredients:Ingredient[]){
+     this.slService.addIngredients(ingredients);
+   }
+}
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from './shopping-list.service';
 
 @Component({
   selector: 'app-shopping-list',
@@ -9,16 +10,17 @@ import { Ingredient } from '../shared/ingredient.model';
 })
 export class ShoppingListComponent implements OnInit {
   ingredients: Ingredient[] = [
-    new Ingredient('Apples', 5),
-    new Ingredient('Tomatoes', 10),
+
   ];
 
-  constructor() { }
+  constructor(private slService: ShoppingListService) { }
 
   ngOnInit() {
+    this.ingredients=this.slService.getIngredients();
+    this.slService.ingredientsChanged.subscribe((ingredients: Ingredient [])=>{
+      this.ingredients=ingredients;
+    })
   }
 
-  onIngredientAdded(ingredient: Ingredient) {
-    this.ingredients.push(ingredient);
-  }
+
 }
