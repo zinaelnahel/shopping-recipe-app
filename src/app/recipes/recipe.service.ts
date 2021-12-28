3a5d4f8378c1e22a8641dc324383a6c7d474f61f
zinaelnahel <zina.el.nahel@accenture.com>
2021-12-28 20:45:44 +0100

    add routing

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,11 +1,12 @@
 
-import { EventEmitter, Injectable } from "@angular/core";
+import {  Injectable } from "@angular/core";
+
 import { Ingredient } from "../shared/ingredient.model";
 import { ShoppingListService } from "../shopping-list/shopping-list.service";
 import { Recipe } from "./recipe.model";
 @Injectable()
 export class RecipeService {
-  recipeSelected= new EventEmitter<Recipe>();
+
 
   private recipes: Recipe[]=[
         new Recipe('Schnitzel', 
@@ -19,7 +20,10 @@ export class RecipeService {
   constructor(private slService : ShoppingListService ){}
    getRecipes(){
      return this.recipes.slice();
-   }   
+   } 
+   getRecipe(index:number )  {
+     return this.recipes[index];
+   }
    addIngredientsToShoppingList(ingredients:Ingredient[]){
      this.slService.addIngredients(ingredients);
    }
